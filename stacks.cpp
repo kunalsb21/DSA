444e7a9ddc155b42f8cdd462ccfc8b8d8882f99e
Kunal Bhajbhuje <kunalsb21@iitk.ac.in>
2024-08-14 15:18:10 +0530

    new prog

--- stacks.cpp
+++ stacks.cpp
@@ -563,6 +563,39 @@ s = {0, 1, 2, 3, 4, 5}: Index of the sixth element is pushed.
 
 ////// Largest rectangular area in a histogram
 
-void
+//// Naive
 
+int getMaxArea(int arr[], int n){
+    int res = 0;
+    for(int i=0 ; i<n; i++){
+        int curr = arr[i];
+        for(int j=i-1; j>=0; j--){
+            if(arr[j]>=arr[i]){
+                curr+=arr[i];
+            }
+            else{
+                break;
+            }
+        }
+         for(int j=i+1; j<n; j++){
+            if(arr[j]>=arr[i]){
+                curr+=arr[i];
+            }
+            else{
+                break;
+            }
+        }
+        res = max(res,curr);
+    }
+    return res;
+}
+
+
+int main() 
+{ 
+    int arr[]={6,2,5,4,1,5,6};
+    int n=7;
+    cout<<"Maximum Area: "<<getMaxArea(arr,n);
+    return 0; 
+}
 
