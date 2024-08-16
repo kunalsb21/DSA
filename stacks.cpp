beddff1075007d55e19b612bf7a8d4eb20089f2c
Kunal Bhajbhuje <kunalsb21@iitk.ac.in>
2024-08-16 12:54:09 +0530

    new update

--- stacks.cpp
+++ stacks.cpp
@@ -774,6 +774,7 @@ s = {0, 1, 2, 3, 4, 5}: Index of the sixth element is pushed.
 // }
 
 
+
 ////  Design a stack that supports getMin()
 
 // struct MyStack {
@@ -828,6 +829,123 @@ s = {0, 1, 2, 3, 4, 5}: Index of the sixth element is pushed.
 
 
 
-//// 
+////  Design a stack with getMin() int O(1) space
+
+//// Method 1: handles only positives
+
+// struct MyStack {
+ 
+//     stack<int> s;
+//     int min;
+ 
+// void push(int x) {
+ 
+//       if(s.empty() ) {
+//          min=x;
+//          s.push(x);
+//       }
+//       else if(x<=min){
+//           s.push(x-min);
+//           min=x;
+//       }else{
+//       s.push(x);
+//       }
+//    }
+ 
+// int pop() {
+
+//     int t=s.top();s.pop();
+//     if(t<=0){
+//         int res=min;
+//         min=min-t;
+//         return res;
+//     }else{
+//         return t;
+//     }
+//    }
+
+// int top() {
+//     int t=s.top();
+//     return ((t<=0)? min : t);
+//    }
+ 
+// int getMin() {
+//       return min;
+//    }
+// };
+
+// int main()
+// {
+//     MyStack s;
+//     s.push(4);
+//     s.push(5);
+//     s.push(8);
+//     s.push(1);
+//     s.pop();
+ 
+//     cout<<" Minimum Element from Stack: " <<s.getMin();
+  
+//     return 0; 
+// }
+
+
+//// Method 2: handles only negatives
+
+// struct MyStack {
+ 
+//     stack<int> s;
+//     int min;
+ 
+// void push(int x) {
+ 
+//       if(s.empty() ) {
+//          min=x;
+//          s.push(x);
+//       }
+//       else if(x<=min){
+//           s.push(2*x-min);
+//           min=x;
+//       }else{
+//       s.push(x);
+//       }
+//    }
+ 
+// int pop() {
+
+//     int t=s.top();s.pop();
+//     if(t<=min){
+//         int res=min;
+//         min=2*min-t;
+//         return res;
+//     }else{
+//         return t;
+//     }
+//    }
+
+// int top() {
+//     int t=s.top();
+//     return ((t<=min)? min : t);
+//    }
+ 
+// int getMin() {
+//       return min;
+//    }
+// };
+
+// int main()
+// {
+//     MyStack s;
+//     s.push(4);
+//     s.push(5);
+//     s.push(8);
+//     s.push(1);
+//     s.pop();
+ 
+//     cout<<" Minimum Element from Stack: " <<s.getMin();
+  
+//     return 0; 
+// }
+
+
 
 
